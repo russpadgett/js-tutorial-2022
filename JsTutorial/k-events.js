9e4d90be3fb62bd23d48f263d62b5c556cf7707d
russpadgett <30752168+russpadgett@users.noreply.github.com>
2022-09-03 22:36:46 -0500

    1 file modified

--- JsTutorial/k-events.js
+++ JsTutorial/k-events.js
@@ -1,6 +1,6 @@
 export default function main(){
   //**************************************************************************
-  // EVENTS *this binds the receiving object (eg. onClick - this is the button clicked)
+  // EVENTS 'this' binds the receiving object (eg. onClick - this is the button clicked)
   return '<button onclick="this.innerHTML = Date()">The time is?</button>'
   
 }
